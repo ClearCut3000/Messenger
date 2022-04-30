4b63c5a524752eeeb46f8d44f5bffab7ec22e46f
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-04-30 22:18:47 +0300

    Login screen subview's & layout added

--- Messenger/Controllers/Login/LoginViewController.swift
+++ Messenger/Controllers/Login/LoginViewController.swift
@@ -10,11 +10,65 @@ import UIKit
 class LoginViewController: UIViewController {
 
   //MARK: - Subview's
+  private let scrollView: UIScrollView = {
+    let scrollView = UIScrollView()
+    scrollView.clipsToBounds = true
+    return scrollView
+  }()
+
   private let imageView: UIImageView = {
     let imageView = UIImageView()
     imageView.image = UIImage(named: "logo")
     imageView.contentMode = .scaleAspectFit
     return imageView
+  }()
+
+  private let emailField: UITextField = {
+    let field = UITextField()
+    field.autocapitalizationType = .none
+    field.autocorrectionType = .no
+    field.returnKeyType = .continue
+    field.layer.cornerRadius = 12
+    field.layer.borderWidth =  1
+    field.layer.borderColor = UIColor.lightGray.cgColor
+    field.placeholder = "Email Address..."
+    field.leftView = UIView(frame: CGRect(x: 0,
+                                          y: 0,
+                                          width: 5,
+                                          height: 0))
+    field.leftViewMode = .always
+    field.backgroundColor = .white
+    return field
+  }()
+
+  private let passwordField: UITextField = {
+    let field = UITextField()
+    field.autocapitalizationType = .none
+    field.autocorrectionType = .no
+    field.returnKeyType = .done
+    field.layer.cornerRadius = 12
+    field.layer.borderWidth =  1
+    field.layer.borderColor = UIColor.lightGray.cgColor
+    field.placeholder = "Password..."
+    field.leftView = UIView(frame: CGRect(x: 0,
+                                          y: 0,
+                                          width: 5,
+                                          height: 0))
+    field.leftViewMode = .always
+    field.backgroundColor = .white
+    field.isSecureTextEntry = true
+    return field
+  }()
+
+  private let loginButton: UIButton = {
+    let button = UIButton()
+    button.setTitle("Log In", for: .normal)
+    button.backgroundColor = .link
+    button.setTitleColor(.white, for: .normal)
+    button.layer.cornerRadius = 12
+    button.layer.masksToBounds = true
+    button.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+    return button
   }()
 
   //MARK: - View Lifecycle
@@ -26,17 +80,35 @@ class LoginViewController: UIViewController {
                                                         style: .done,
                                                         target: self,
                                                         action: #selector(didTapRegister))
-    view.addSubview(imageView)
+    //Adding subviews
+    view.addSubview(scrollView)
+    scrollView.addSubview(imageView)
+    scrollView.addSubview(emailField)
+    scrollView.addSubview(passwordField)
+    scrollView.addSubview(loginButton)
   }
 
   //MARK: - Layout
   override func viewDidLayoutSubviews() {
     super.viewDidLayoutSubviews()
-    let size = view.width/3
-    imageView.frame = CGRect(x: (view.width - size)/2,
-                             y: 20,
-                             width: size,
-                             height: size)
+    scrollView.frame    = view.bounds
+    let size            = scrollView.width/3
+    imageView.frame     = CGRect(x: (scrollView.width - size)/2,
+                                 y: 20,
+                                 width: size,
+                                 height: size)
+    emailField.frame    = CGRect(x: 30,
+                                 y: imageView.bottom+10,
+                                 width: scrollView.width-60,
+                                 height: 52)
+    passwordField.frame = CGRect(x: 30,
+                                 y: emailField.bottom+10,
+                                 width: scrollView.width-60,
+                                 height: 52)
+    loginButton.frame   = CGRect(x: 30,
+                                 y: passwordField.bottom+10,
+                                 width: scrollView.width-60,
+                                 height: 52)
   }
 
   //MARK: - Actions
