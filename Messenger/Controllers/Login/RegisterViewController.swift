a40f72d29a564d578030ba5a63a95304febfde4b
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-05-05 21:08:24 +0300

    Database Set Up

--- Messenger/Controllers/Login/RegisterViewController.swift
+++ Messenger/Controllers/Login/RegisterViewController.swift
@@ -197,17 +197,23 @@ class RegisterViewController: UIViewController {
             return
           }
     //Firebase Account Creation
-    FirebaseAuth.Auth.auth().createUser(withEmail: email, password: password) { [weak self] authResult, error in
+    DatabaseManager.shared.userExists(with: email) { [weak self] exists in
       guard let strongSelf = self else { return }
-      guard let result = authResult, error == nil else { return }
-      let user = result.user
-      print("Created User: \(user)")
-      strongSelf.navigationController?.dismiss(animated: true, completion: nil)
+      guard !exists else {
+        // user already exists
+        strongSelf.alertUserLoginError(message: "Looks like a user account for that email address already exists.")
+        return
+      }
+      FirebaseAuth.Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+        guard authResult != nil, error == nil else { return }
+        DatabaseManager.shared.insertUser(with: ChatAppUser(firstName: firstName, lastName: lastName, emailAddress: email))
+        strongSelf.navigationController?.dismiss(animated: true, completion: nil)
+      }
     }
   }
 
-  func alertUserLoginError() {
-    let alert = UIAlertController(title: "Woops!", message: "Please enter all information to create new account.", preferredStyle: .alert)
+  func alertUserLoginError(message: String = "Please enter all information to create new account.") {
+    let alert = UIAlertController(title: "Woops!", message: message, preferredStyle: .alert)
     alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
     present(alert, animated: true)
   }
--- Messenger/Supported Files/DatabaseManager.swift
+++ Messenger/Supported Files/DatabaseManager.swift
@@ -17,3 +17,31 @@ final class DatabaseManager {
   //MARK: - Methods
 
 }
+
+//MARK: - Account Management
+extension DatabaseManager {
+  public func userExists(with email: String, completion: @escaping ((Bool) -> Void)) {
+    database.child(email).observeSingleEvent(of: .value) { snapshot in
+      guard snapshot.value as? String != nil else {
+        completion(false)
+        return
+      }
+      completion(true)
+    }
+  }
+
+  /// Inserts new user in to database
+  public func insertUser(with user: ChatAppUser) {
+    database.child(user.emailAddress).setValue([
+      "first_name": user.firstName,
+      "last_name": user.lastName
+    ])
+  }
+}
+
+struct ChatAppUser {
+  let firstName: String
+  let lastName: String
+  let emailAddress: String
+//  let profilePictureUrl: String
+}
