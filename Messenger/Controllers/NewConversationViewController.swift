eaa3ba1767626a1dfef32b8c66699251175e6fed
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-06-01 22:44:06 +0300

    NewConversationCell class added for ConversationsViewController to show filtered user's on search controller

--- Messenger/Controllers/NewConversationViewController.swift
+++ Messenger/Controllers/NewConversationViewController.swift
@@ -13,8 +13,8 @@ class NewConversationViewController: UIViewController {
   //MARK: - Properties
   private var users = [[String: String]]()
   private var hasFetched = false
-  private var results = [[String: String]]()
-  public var completion: (([String: String]) -> (Void))?
+  private var results = [SearchResult]()
+  public var completion: ((SearchResult) -> (Void))?
 
   //MARK: - Subview's
   private let spinner = JGProgressHUD(style: .dark)
@@ -28,7 +28,7 @@ class NewConversationViewController: UIViewController {
   private let tableView: UITableView = {
     let table = UITableView()
     table.isHidden = true
-    table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    table.register(NewConversationCell.self, forCellReuseIdentifier: NewConversationCell.identifier)
     return table
   }()
 
@@ -83,8 +83,9 @@ extension NewConversationViewController: UITableViewDelegate, UITableViewDataSou
   }
 
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-    cell.textLabel?.text = results[indexPath.row]["name"]
+    let model = results[indexPath.row]
+    let cell = tableView.dequeueReusableCell(withIdentifier: NewConversationCell.identifier, for: indexPath) as! NewConversationCell
+    cell.configure(with: model)
     return cell
   }
 
@@ -95,6 +96,10 @@ extension NewConversationViewController: UITableViewDelegate, UITableViewDataSou
     dismiss(animated: true) { [weak self] in
       self?.completion?(targetUserData)
     }
+  }
+
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 90
   }
 }
 
@@ -130,11 +135,17 @@ extension NewConversationViewController: UISearchBarDelegate {
 
   func filterUsers(with term: String) {
     //update UI: either show results or show no results label
-    guard hasFetched else { return }
+    guard let currentUserEmail = UserDefaults.standard.value(forKey: "email") as? String, hasFetched else { return }
+    let safeEmail = DatabaseManager.safeEmail(emailAddress: currentUserEmail)
     self.spinner.dismiss()
-    let results: [[String: String]] = self.users.filter {
+    let results: [SearchResult] = self.users.filter {
+      guard let email = $0["email"],
+            email != safeEmail else { return false }
       guard let name = $0["name"]?.lowercased() else { return false }
       return name.hasPrefix(term.lowercased())
+    }.compactMap {
+      guard let name = $0["name"], let email = $0["email"] else { return nil }
+      return SearchResult(name: name, email: email)
     }
     self.results = results
     updateUI()
@@ -150,4 +161,9 @@ extension NewConversationViewController: UISearchBarDelegate {
       self.tableView.reloadData()
     }
   }
+}
+
+struct SearchResult {
+  let name: String
+  let email: String
 }
---
+++ Messenger/Views/NewConversationCell.swift
@@ -0,0 +1,70 @@
+//
+//  NewConversationCell.swift
+//  Messenger
+//
+//  Created by Николай Никитин on 01.06.2022.
+//
+
+import Foundation
+import SDWebImage
+
+class NewConversationCell: UITableViewCell {
+
+  //MARK: - Properties
+  static let identifier = "NewConversationCell"
+
+  //MARK: - Subview's
+  private let userImageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.contentMode = .scaleAspectFill
+    imageView.layer.cornerRadius = 35
+    imageView.layer.masksToBounds = true
+    return imageView
+  }()
+
+  private let userNameLabel: UILabel = {
+    let label = UILabel()
+    label.font = .systemFont(ofSize: 21, weight: .semibold)
+    return label
+  }()
+
+  //MARK: - Init's
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    contentView.addSubview(userImageView)
+    contentView.addSubview(userNameLabel)
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  //MARK: - Layout
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    userImageView.frame   = CGRect(x: 10,
+                                   y: 10,
+                                   width: 70,
+                                   height: 70 )
+    userNameLabel.frame    = CGRect(x: userImageView.right + 10,
+                                    y: 20,
+                                    width: contentView.width - 20 - userImageView.width,
+                                    height: 50)
+  }
+
+  //MARK: - Methods
+  public func configure(with model: SearchResult) {
+    self.userNameLabel.text = model.name
+    let path = "images/\(model.email)_profile_picture.png"
+    StorageManager.shared.downloadURL(for: path) { [weak self] result in
+      switch result {
+      case .success(let url):
+        DispatchQueue.main.async {
+          self?.userImageView.sd_setImage(with: url, completed: nil)
+        }
+      case .failure(let error):
+        print("Failed to get image URL - \(error)")
+      }
+    }
+  }
+}
